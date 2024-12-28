387b2dde6437f6f8c282932e2f368af0d05f423d
Alex Rutkovskiy <alexrutkowsky@tut.by>
2024-12-29 00:10:16 +0300

    Feat: add utils to load levels

---
+++ src/shared/types/level.ts
@@ -0,0 +1,5 @@
+export interface ILevel {
+    words: string[];
+}
+
+export type ILevels = Record<number, ILevel>;
---
+++ src/shared/utils/loadLevels.ts
@@ -0,0 +1,20 @@
+import { ILevels, ILevel } from "@shared/types/level";
+
+export async function loadLevels(): Promise<ILevels> {
+    const res = {} as ILevels;
+
+    try {
+        const data = import.meta.glob('@shared/assets/dictionary/*.json');
+        let index = 0;
+
+        for (const p in data) {
+            const value = await data[p]();
+            res[index] = {words: (value as ILevel).words};
+            index++;
+        }
+    } catch(e) {
+        console.log(e)
+    }
+
+    return res;
+}
