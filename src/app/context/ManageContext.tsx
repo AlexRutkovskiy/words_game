45bb112a23d8ee3dc64a310790f2c9425a2e9f0c
Alex Rutkovskiy <alexrutkowsky@tut.by>
2025-01-01 23:47:26 +0300

    Feat: add methods to manage context

--- src/app/context/ManageContext.tsx
+++ src/app/context/ManageContext.tsx
@@ -6,5 +6,12 @@ import { LEVEL_POINTS } from '@shared/consts/level';
 export const ManageContext = createContext<IManageProvider>({
     currentLevel: LEVEL_POINTS.START,
     finished: false,
-    startLevel: () => {}
+    startLevel: () => {},
+    guessedWords: [],
+    addGuessedWord: (word: string) => {console.log(word)},
+    updateFromStorage: (level: string, finished: boolean, guessedWords: string[]) => {
+        console.log(level);
+        console.log(finished);
+        console.log(guessedWords);
+    }
 })
--- src/shared/types/manageProvider.ts
+++ src/shared/types/manageProvider.ts
@@ -2,4 +2,7 @@ export interface IManageProvider {
     currentLevel: string;
     finished: boolean;
     startLevel: ( nextLevel: string ) => void;
+    guessedWords: string[];
+    addGuessedWord: (word: string) => void;
+    updateFromStorage: (level: string, finished: boolean, guessedWords: string[]) => void
 }
